76e6ce45ae4cc4d4a6fb5c1adfea6de4faf6cd1c
ALCstkgit <adrian.lopezc@softtek.com>
2023-05-17 11:19:35 +0200

    dia3v5

--- WorkSpace/FundamentosSpring/src/main/java/com/softtek/fundamentosspring/modelTaller/CocheTaller.java
+++ WorkSpace/FundamentosSpring/src/main/java/com/softtek/fundamentosspring/modelTaller/CocheTaller.java
@@ -1,27 +1,12 @@
 package com.softtek.fundamentosspring.modelTaller;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class CocheTaller {
     private String matricula;
     private String modelo;
-    
-    public CocheTaller(String matricula, String modelo){
-        this.matricula = matricula;
-        this.modelo = modelo;
-    }
-
-    public String getMatricula() {
-        return matricula;
-    }
-
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
 }
