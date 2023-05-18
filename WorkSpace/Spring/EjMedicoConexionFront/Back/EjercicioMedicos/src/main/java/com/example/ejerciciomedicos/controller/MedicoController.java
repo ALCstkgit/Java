f101035939cd951a69bfa70dc68c22044a7be23a
ALCstkgit <adrian.lopezc@softtek.com>
2023-05-18 13:27:38 +0200

    dia4v3

--- WorkSpace/Spring/EjMedicoConexionFront/Back/EjercicioMedicos/src/main/java/com/example/ejerciciomedicos/controller/MedicoController.java
+++ WorkSpace/Spring/EjMedicoConexionFront/Back/EjercicioMedicos/src/main/java/com/example/ejerciciomedicos/controller/MedicoController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/medicos")
@@ -16,7 +18,7 @@ public class MedicoController {
     IMedicoService ms;
 
     @GetMapping
-    public Medico[] listMedicos() throws Exception{
+    public List<Medico> listMedicos() throws Exception{
         return ms.listMedicos();
     }
 }
