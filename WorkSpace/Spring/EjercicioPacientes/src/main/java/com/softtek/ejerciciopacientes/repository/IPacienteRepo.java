c2067130f3c0cbdadf7b8754d921dfab3bd77795
ALCstkgit <adrian.lopezc@softtek.com>
2023-05-22 11:10:05 +0200

    dia3v7

--- WorkSpace/Spring/EjercicioPacientes/src/main/java/com/softtek/ejerciciopacientes/repository/IPacienteRepo.java
+++ WorkSpace/Spring/EjercicioPacientes/src/main/java/com/softtek/ejerciciopacientes/repository/IPacienteRepo.java
@@ -2,9 +2,15 @@ package com.softtek.ejerciciopacientes.repository;
 
 import com.softtek.ejerciciopacientes.model.Paciente;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public interface IPacienteRepo {
-    Paciente[]pacientes = {new Paciente(1,"Juan",20),
-    new Paciente(2,"Laura",30),
-    new Paciente(3,"Pedro",65)};
+    List<Paciente>[]pacientes = new List[]{Arrays.asList(
+            new Paciente(1, "Juan", 20),
+            new Paciente(2, "Laura", 30),
+            new Paciente(3, "Pedro", 65)
+    )};
     String obtenerUno(Paciente p1);
 }
